489bc83c3aff35806ca48970bc86075f4c511c1e
Ryan Le <ryanle@live.com>
2020-07-15 13:53:12 -0500

    Improved mobile support for sidebar.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { 
   Grid,
-  Drawer, 
+  Drawer,
+  SwipeableDrawer,
   Divider,
   List,
   ListItem,
@@ -96,6 +97,10 @@ const useStyles = makeStyles((theme) => ({
   content: {
     flexGrow: 1,
   },
+  link: {
+    color: "white",
+    textDecoration: "none",
+  }
 }));
 
 const Sidebar = (props) => {
@@ -118,7 +123,7 @@ const Sidebar = (props) => {
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
-  }
+  };
   
   const sidebar = (
     <div>
@@ -249,6 +254,118 @@ const Sidebar = (props) => {
     </div>
   );
 
+  const mobileSidebar = (
+    <div>
+      <div style={{ textAlign: "center", margin: 15, }}>
+        <Typography variant="h5" className={classes.whiteText}>Ryan Le</Typography>
+        <Typography variant="body1" className={classes.whiteText}>Software Engineer</Typography>
+        <br />
+        <Typography variant="body1" className={classes.whiteText}>Hey, nice phone!</Typography>
+      </div>
+      <p/>
+
+      <Divider className={classes.divider}/>
+
+      {/* Navigation */}
+      <List component="nav" className={classes.whiteText}>
+        <ListItem button
+          classes={{ selected: classes.active }}
+          component={Link}
+          to="/"
+          onClick={() => {setSelected(nav.HOME)}}
+          selected={selected === nav.HOME}
+        >
+          <ListItemIcon><Home className={classes.navIcon}/></ListItemIcon>
+          <ListItemText primary="Home"/>
+        </ListItem>
+
+        <ListItem button
+          classes={{ selected: classes.active }}
+          component={Link}
+          to="/about-me"
+          onClick={() => {setSelected(nav.ABOUT)}}
+          selected={selected === nav.ABOUT}
+        >
+          <ListItemIcon><Person className={classes.navIcon}/></ListItemIcon>
+          <ListItemText primary="About"/>
+        </ListItem>
+
+        <ListItem button
+          classes={{ selected: classes.active }}
+          component={Link}
+          to="/experience"
+          onClick={() => {setSelected(nav.EXPERIENCE)}}
+          selected={selected === nav.EXPERIENCE}
+        >
+          <ListItemIcon><Work className={classes.navIcon}/></ListItemIcon>
+          <ListItemText primary="Experience"/>
+        </ListItem>
+
+        <ListItem button
+          classes={{ selected: classes.active }}
+          component={Link}
+          to="/skills"
+          onClick={() => {setSelected(nav.SKILLS)}}
+          selected={selected === nav.SKILLS}
+        >
+          <ListItemIcon><Code className={classes.navIcon}/></ListItemIcon>
+          <ListItemText primary="Skills"/>
+        </ListItem>
+
+        <ListItem button
+          classes={{ selected: classes.active }}
+          component={Link}
+          to="/projects"
+          onClick={() => {setSelected(nav.PROJECTS)}}
+          selected={selected === nav.PROJECTS}
+        >
+          <ListItemIcon><Computer className={classes.navIcon}/></ListItemIcon>
+          <ListItemText primary="Projects"/>
+        </ListItem>
+
+        <ListItem button
+          classes={{ selected: classes.active }}
+          component={Link}
+          to="/contact-me"
+          onClick={() => {setSelected(nav.CONTACT)}}
+          selected={selected === nav.CONTACT}
+        >
+          <ListItemIcon><Email className={classes.navIcon}/></ListItemIcon>
+          <ListItemText primary="Contact"/>
+        </ListItem>
+        
+      </List>
+
+      <Divider className={classes.divider}/>
+
+      <Typography variant="body1" className={classes.whiteText} style={{ textAlign: "center", paddingTop: 10 }}>Other Platforms</Typography>
+
+      <List component="nav" className={classes.whiteText}>
+        <a href="https://github.com/ryansle" className={classes.link}>
+          <ListItem button>
+          <ListItemIcon><GitHub className={classes.navIcon}/></ListItemIcon>
+            <ListItemText primary="GitHub"/>
+          </ListItem>
+        </a>
+        <a href="https://linkedin.com/in/ryansle" className={classes.link}>
+          <ListItem button>
+          <ListItemIcon><LinkedIn className={classes.navIcon}/></ListItemIcon>
+            <ListItemText primary="LinkedIn"/>
+          </ListItem>
+        </a>
+        <a href="/" className={classes.link}>
+          <ListItem button>
+          <ListItemIcon><Description className={classes.navIcon}/></ListItemIcon>
+            <ListItemText primary="Résumé"/>
+          </ListItem>
+        </a>
+      </List>
+
+      <Divider className={classes.divider}/>
+      
+    </div>
+  );
+
   return (
     <div className={classes.root}>
       <AppBar position="fixed" className={classes.appBar}>
@@ -262,14 +379,12 @@ const Sidebar = (props) => {
           >
             <Menu />
           </IconButton>
-          <Typography variant="h6" noWrap>
-            Responsive Sidebar
-          </Typography>
+          <Typography variant="h5" noWrap>{selected}</Typography>
         </Toolbar>
       </AppBar>
       <nav className={classes.drawer} aria-label="navigation menu">
         <Hidden smUp implementation="css">
-          <Drawer
+          <SwipeableDrawer
             className={classes.sidebar}
             container={container}
             variant="temporary"
@@ -283,8 +398,8 @@ const Sidebar = (props) => {
               keepMounted: true, // Better open performance on mobile.
             }}
           >
-            {sidebar}
-          </Drawer>
+            {mobileSidebar}
+          </SwipeableDrawer>
         </Hidden>
         <Hidden xsDown implementation="css">
           <Drawer
