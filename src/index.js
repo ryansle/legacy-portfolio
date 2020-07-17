05dcf479ea3eef3a5a393dd73bfbb9292c002f06
Ryan Le <ryanle@live.com>
2020-07-17 13:22:33 -0500

    Fixed a bug where pages started at the same scroll distance as the previous page

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
+import ScrollToTop from "./ScrollToTop";
 
 ReactDOM.render(
   <Router>
+    <ScrollToTop />
     <React.StrictMode>
       <App />
     </React.StrictMode>,
