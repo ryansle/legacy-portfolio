8e2f12b223fb6673edc9bc3b5bdeb725fbb48639
Ryan Le <ryanle@live.com>
2020-08-19 18:57:00 -0500

    Updated Re-Logic page

--- src/sections/ReLogic.jsx
+++ src/sections/ReLogic.jsx
@@ -3,6 +3,7 @@ import React from "react";
 // Components
 import {
   Typography,
+  Grid,
   useMediaQuery
 } from "@material-ui/core";
 import { Link } from "react-router-dom";
@@ -14,6 +15,7 @@ import { makeStyles } from "@material-ui/core/styles";
 // Assets
 import relogic from "../resources/experience/re-logic.png";
 import credits from "../resources/experience/credits.png";
+import terraria from "../resources/experience/terraria.jpeg";
 
 const useStyles = makeStyles(() => ({
   media: {
@@ -61,29 +63,43 @@ const ReLogic = () => {
           Let's address the frequently asked questions, and if you have any more, feel free to <Link to="/contact">contact me!</Link>
         </Typography>
         <br/>
-        <Typography variant="h5" className="subtitle">What is Terraria?</Typography>
-        <br/>
-        <Typography variant="body1">
-          Terraria is a 2D sandbox game with gameplay that revolves around exploration, 
-          building, crafting, combat, and mining, playable in both single-player and 
-          multiplayer modes. The game has a 2D sprite tile-based graphical style 
-          reminiscent of the 16-bit sprites found on the Super NES. 
-          The game is noted for its classic exploration-adventure style of gameplay, 
-          similar to games such as the Metroid series and Minecraft.
-        </Typography>
+        <Grid container>
+          <Grid item xs={isSmallScreen ? 12 : 6}>
+            <Typography variant="h5" className="subtitle">What is Terraria?</Typography>
+            <br/>
+            <Typography variant="body1">
+              Terraria is a 2D sandbox game with gameplay that revolves around exploration, 
+              building, crafting, combat, and mining, playable in both single-player and 
+              multiplayer modes. The game has a 2D sprite tile-based graphical style 
+              reminiscent of the 16-bit sprites found on the Super NES. 
+              The game is noted for its classic exploration-adventure style of gameplay, 
+              similar to games such as the Metroid series and Minecraft.
+            </Typography>
+          </Grid>
+          <Grid item xs={isSmallScreen ? 12 : 6}>
+            <p align="middle">
+              <img
+                src={terraria}
+                alt="Terraria Gameplay Screenshot"
+                width={isSmallScreen ? "100%" : "70%"}
+                height="auto"
+              />
+            </p>
+          </Grid>
+        </Grid>
         <br/>
         <Typography variant="h5" className="subtitle">How on Earth did I find myself with a job here at age 12?</Typography>
         <br/>
         <Typography variant="body1">
-          When I was younger, I was really, really into Nintendo games. One of my favorite things
+          When I was younger, I had an excessive interest in Nintendo games. One of my favorite things
           to do was create my own Super Mario levels in a decade-old fan game that some people
-          might be familiar with called Super Mario Flash. Around this same time, I discovered a
-          different, much improved Mario fan game entitled Super Mario Brothers X (SMBX for short).
+          might be familiar with called <a href="http://www.pouetpu-games.com/index.php?section=2&game_id=1">Super Mario Flash</a>. Around this same time, I discovered a
+          different, much more improved Mario fan game entitled <a href="https://www.supermariobrosx.org/">Super Mario Brothers X</a> (SMBX for short).
         </Typography>
         <br/>
         <Typography variant="body1">
           SMBX was an independent development project that had absolutely no ties
-          to Nintendo, and it had a sole developer named Andrew Spinks - or 'Redigit', online. The game
+          to Nintendo, and it had a sole developer named Andrew Spinks - or "Redigit", online. The game
           managed to become popular enough to become the center of its own online community at <a href="supermariobrothers.org"> supermariobrothers.org</a>, which has now been repossessed by 
           Nintendo. I was an active member in this community 
           since I enjoyed creating my own Mario levels, which eventually turned me into a prominent member
@@ -94,15 +110,15 @@ const ReLogic = () => {
         <br/>
         <Typography variant="body1">
           But as time went on, Andrew Spinks began to lose interest in continued development of SMBX.
-          This, coupled with increasing pressure from Nintendo via contact from their lawyers eventually 
+          This, coupled with increasing pressure from Nintendo via contact from their lawyers, eventually 
           caused him to cease development of the game in late 2010, but the community lived on.
         </Typography>
         <br/>
         <Typography variant="body1">
           Just a few short months later, I received a message on Facebook from one of my former team members
           on SMBX about "Andrew's newest project," and how they wanted me involved again. Without having the slightest
-          idea of what I was signing myself up for, I agreed to rejoin the team and was quickly reintegrated into all 
-          of the team's secret IRC channels, email chains, Dropbox file storages, etc. I was given a rundown of Andrew's new 
+          idea of what was to come, I agreed to rejoin the team and was quickly reintegrated into all 
+          of the team's secret internet relay chat channels, email chains, Dropbox file storages, etc. I was given a rundown of Andrew's new 
           game that he was entitling "Terraria", given a download link, and told to just start playing around with the game.
           For how little content there was at the time, it was incredible, and quickly became the main way I would spend my free time.
         </Typography>
@@ -136,26 +152,26 @@ const ReLogic = () => {
         <Typography variant="h5" className="subtitle">Is Terraria still alive?</Typography>
         <br/>
         <Typography variant="body1">
-          Yes, very much so! While I'm not an active part of the team anymore, those who remain just wrapped up work on the 1.4 Journey's End Update, which has added thousands of
+          Yes, very much so! While I'm not an active part of the team anymore, those who remain just wrapped up work on the <b>v1.4 Journey's End Update</b>, which has added thousands of
           new improvements to the base game. Legend has it that 1.4 is the "final update to Terraria," but if you want to take it from someone who worked for Re-Logic for 
           four years, I doubt that is actually the case. Andrew and his team of developers liked to call many previous updates the "final update" to the game, yet here they are
-          still working on the game today. If I had to make an educated guess, I would say that the game will be ripe with major updates to come in the distant future.
+          still working on the game today. If I had to guess, I would say that the game will be ripe with major updates to come in the distant future.
         </Typography>
         <br/>
         <Typography variant="h5" className="subtitle">Why did I leave the team?</Typography>
         <br/>
         <Typography variant="body1">
           I was 12 years old when I started this position. I worked in it until the summer of 2015, wrapping up my time with Re-Logic immediately following the official release
-          of 1.3, the Endgame Update. As I was now only 16 years old, I wanted time to spend with my friends and family, and just time to spend being a kid. In those four years, I
-          racked up around 1200 hours of game time, and that's only what's been tracked on Steam. I've had tens, if not hundreds, of beta versions of the game installed straight to my
+          of 1.3, the Endgame Update. As I was now only 16 years old, I wanted time to spend with my friends and family, and just time to spend being a kid, free of major responsibility. In those four years, I
+          racked up around 1200 hours of game time, but that's only what Steam has been able to track. I've had tens, if not hundreds, of beta versions of the game installed straight to my
           machine prior to launching on Steam and throughout the years. If I had to pinpoint a true estimate to how many hours I've spent on Terraria, I would estimate it's closer to 1800.
-          That's nearly 2.5 <b>months</b> of game time - as you can see, I was quite dedicated to my position.
+          That's nearly 2.5 <b>months</b> of game time - I was pretty dedicated to my position playtesting the game.
         </Typography>
         <br/>
         <Typography>
-          But I couldn't spend all of my time playing the game forever. So, I finally said goodbye to the Re-Logic team back in high school so I could focus on my friends and family.
-          Sometimes, I still wonder what it would be like if I had stayed. Now that I'm majoring in Software Engineering, and have a basis in computer programming, could I have 
-          contributed to the Re-Logic team in a different medium? Could I have turned Re-Logic Games into a full-time opportunity?
+          But I couldn't spend all of my time playing the game forever. So, I finally said goodbye to the Re-Logic team back in high school so I could spend time doing other things.
+          I often wonder how different things may be had I stayed on with Re-Logic. Now that I am majoring in Software Engineering, could I have pivoted my position with Re-Logic to become
+          a developer on one of the biggest video games of the decade? Could I have turned my tenure with Re-Logic Games into a full-time opportunity?
         </Typography>
         <br/>
         <Typography variant="h5" className="subtitle">Do you have any proof of your involvement?</Typography>
